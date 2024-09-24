3cc094acc72bd63ef443a79a9b9959c2b922cea1
raquelsciencedate <lopesadvogados1965@gmail.com>
2024-09-24 19:52:26 -0300

    versão 1.0

---
+++ app.py
@@ -0,0 +1,26 @@
+#declaracao de variaveis
+nome = input("Informe o nome do usuário: ")
+cpf = int(input("Por favor, informe o seu CPF (somente números): "))
+telefone = int(input("Por favor, informe o seu telefone (somente números): "))
+email = input("Informe o email do usuário: ")
+endereco = input("Informe o endereço do usuário: ")
+genero = input("Informe o gênero do usuário: ")
+escolaridade = input("Informe a esolaridade do usuário: ")
+signo = input("Informe o signo do usuário: ")
+tipo_sanguineo = input("Informe o tipo sanguíneo do usuário: ")
+
+print(nome)
+print(cpf)
+print(telefone)
+print(email)
+print(endereco)
+print(genero)
+print(escolaridade)
+print(signo)
+print(tipo_sanguineo)
+    
+
+
+
+
+  
